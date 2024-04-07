1ee758fe330ef679a9b32e0e9bcc626153af36ec
Vineetha785 B <bandlavineetha1579@gmail.com>
2024-04-07 09:08:43 +0000

    routes and templates and oultet

--- ArtGalleryFrontend/src/main.jsx
+++ ArtGalleryFrontend/src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import HomePage from './Home.jsx';
+import ArtworksPage from './Artworks.jsx';
+import Error404 from './Error404.jsx';
+import FavouritesPage from './Favouties.jsx';
+
+// CSS styles to make the root element occupy 100% width
+const rootStyle = {
+  width: '100%',
+  minHeight: '100vh',
+  margin: 0,
+  padding: 0,
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <div style={rootStyle}> {/* Apply styles to the root element */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} errorElement={<Error404/>}>
+            <Route path="home" element={<HomePage />} />
+            <Route path="favourites" element={<FavouritesPage/>} />
+            <Route path="artworks" element={<ArtworksPage />} />
+            <Route path="global" element={<div />} />
+            <Route path="*" element={<Error404 />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  </React.StrictMode>
+);
